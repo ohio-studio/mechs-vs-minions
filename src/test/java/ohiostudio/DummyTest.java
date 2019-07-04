9ed82d5e14ec0de07b3d51980a55c7eed8525928
Gancarz, Wojciech <wojciech.gancarz@gmail.com>
2019-07-04 16:51:14 +0200

    Added dummy test.

---
+++ src/test/java/ohiostudio/DummyTest.java
@@ -0,0 +1,20 @@
+package ohiostudio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest
+{
+	@Test
+	void succeed()
+	{
+		assertTrue(true);
+	}
+
+	@Test
+	void fail()
+	{
+		assertTrue(false);
+	}
+}
