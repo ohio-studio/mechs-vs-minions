aab89c2c1b255b9feb71c8049aa5351d7322bb99
Wojciech Gancarz <wojciech.gancarz@gmail.com>
2019-07-13 16:12:26 +0200

    Added example Main and test.

---
+++ src/main/java/pl/ohiostudio/mechsvsminions/Main.java
@@ -0,0 +1,7 @@
+package pl.ohiostudio.mechsvsminions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mechs vs Minions.");
+    }
+}
---
+++ src/test/java/pl/ohiostudio/mechsvsminions/ExampleTest.java
@@ -0,0 +1,14 @@
+package pl.ohiostudio.mechsvsminions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void shouldCalculateCorrectly() {
+        Integer result = 2 * 2;
+        assertEquals(4, result);
+    }
+}
